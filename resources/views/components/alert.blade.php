72cc425e388135a60fbe62ffa112db22fc35ae0a
mrgerson <gcalvo@tecbaco.com>
2023-09-28 14:02:46 -0500

    configuracion inicial

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,7 @@
+<div
+  class="mb-4 rounded-lg bg-danger-100 px-6 py-5 text-base text-danger-700"
+  role="alert">
+  A simple danger alert with
+  <a href="#!" class="font-bold text-danger-700">an example link</a>. Give
+  it a click if you like.
+</div>

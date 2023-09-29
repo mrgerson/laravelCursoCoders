4e2a290217b2d81244773e82f98de3372ea0761d
mrgerson <gcalvo@tecbaco.com>
2023-09-29 09:06:09 -0500

    vistas blade

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,8 @@
-<div
-  class="mb-4 rounded-lg bg-danger-100 px-6 py-5 text-base text-danger-700"
-  role="alert">
-  A simple danger alert with
-  <a href="#!" class="font-bold text-danger-700">an example link</a>. Give
-  it a click if you like.
-</div>
+<div role="alert">
+    <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+      Danger
+    </div>
+    <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+      <p>Something not ideal might be happening.</p>
+    </div>
+  </div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+{{-- <x-app-layout> --}}
+
+@extends('layouts.app2')
+
+@section('title', 'laravel')
+
+@section('content')
+    <h1>bienvenido</h1>
+    <x-alert>
+
+    </x-alert>
+
+@endsection
+
+{{-- </x-app-layout> --}}
